f21f0bf70441e1f8eff3da8a7e9ea45e6145ecd0
Kristaps Taube <kristaps@ktaube.com>
2025-03-16 17:59:40 +0200

    feat: seed db

---
+++ app-api/scripts/seed.ts
@@ -0,0 +1,19 @@
+import casual from "casual";
+import { db } from "../src/db";
+import { addresses } from "../src/db/schema";
+
+async function main() {
+  for (let id = 1; id < 20; id++) {
+    await db.insert(addresses).values({
+      id,
+      address: casual.address1,
+      country: casual.country,
+      zip: casual.zip({ digits: 5 }),
+    });
+  }
+}
+
+main().catch(async (e) => {
+  console.error(e);
+  process.exit(1);
+});

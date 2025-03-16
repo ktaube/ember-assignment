7c20c3079bcbb47b29dc75c2ed24f109998fb704
Kristaps Taube <kristaps@ktaube.com>
2025-03-16 20:09:47 +0200

    feat: ember logo

--- app-ui/src/routes/-components/navbar.tsx
+++ app-ui/src/routes/-components/navbar.tsx
@@ -5,20 +5,16 @@ export default function Navbar() {
     <header className="sticky top-0 z-50 w-full border-b bg-background">
       <div className="container flex h-16 items-center px-4 mx-auto">
         <div className="flex items-center gap-2">
-          <span className="font-semibold">AddressDB</span>
+          <span className="font-semibold">
+            <Link to="/">
+              <img
+                src="/ember.avif"
+                alt="AddressDB"
+                className="object-cover  h-10"
+              />
+            </Link>
+          </span>
         </div>
-        <nav className="ml-8">
-          <ul className="flex gap-6">
-            <li>
-              <Link
-                to="/"
-                className="text-sm font-medium bg-primary/10 text-primary px-3 py-1.5 rounded-full"
-              >
-                Addresses
-              </Link>
-            </li>
-          </ul>
-        </nav>
       </div>
     </header>
   );
